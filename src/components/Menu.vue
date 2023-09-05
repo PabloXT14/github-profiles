e71ad6dd241793fe2dd041592ceddac3572a7c10
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 12:28:19 -0300

    feat: using githubStore on Menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,33 +3,59 @@
 import { RouterLink, useRouter } from 'vue-router';
 
 import Tag from './Tag.vue';
-import { ref } from 'vue';
+import { useGitHubStore } from '@/stores/GitHubStore';
+import { onMounted } from 'vue';
 
-const activeTab = ref<'repositories' | 'starred'>('repositories');
+const router = useRouter();
+
+const currentRoute = router.currentRoute.value.name
+
+const githubStore = useGitHubStore();
+
+async function handleSwitchRoute() {
+  if (currentRoute === 'starred') {
+    await githubStore.fetchRepositories()
+    await router.push('/repositories')
+  }
+
+  if (currentRoute === 'repositories') {
+    await githubStore.fetchStarredRepositories()
+    await router.push('/starred')
+  }
+}
+
+onMounted(async () => {
+  await githubStore.getTotals()
+})
 
 </script>
 
 <template>
   <nav class="flex w-full border-b border-b-zinc-500 h-12">
-    <RouterLink
-      to="/repositories"
+    <button
+      @click="handleSwitchRoute()"
       class="flex items-center px-4 gap-2 h-full border-b-4"
-      :class="[activeTab === 'repositories' ? 'border-b-red-400' : 'border-b-transparent']"
+      :class="[currentRoute === 'repositories' ? 'border-b-red-400' : 'border-b-transparent']"
     >
       <span class="text-zinc-200 text-xl font-bold">
         Repositórios
       </span>
-      <Tag variant="gray">30</Tag>
-    </RouterLink>
-    <RouterLink
-      to="/starred"
+      <Tag variant="gray">
+        {{ githubStore.totalRepositories }}
+      </Tag>
+    </button>
+
+    <button
+      @click="handleSwitchRoute()"
       class="flex items-center px-4 gap-2 h-full border-b-4"
-      :class="[activeTab === 'starred' ? 'border-b-red-400' : 'border-b-transparent']"
+      :class="[currentRoute === 'starred' ? 'border-b-red-400' : 'border-b-transparent']"
     >
       <span class="text-zinc-200 text-xl font-bold">
         Starred
       </span>
-      <Tag variant="gray">30</Tag>
-    </RouterLink>
+      <Tag variant="gray">
+        {{ githubStore.totalStarredRepositories }}
+      </Tag>
+    </button>
   </nav>
 </template>

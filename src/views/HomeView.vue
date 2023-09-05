cc7f2ea00cd14176c5d0c95e5572dbd777dc7852
Pablo Alan <pabloxt14@gmail.com>
2023-09-04 22:47:01 -0300

    feat: install iconify library icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
   <main>
     <h1>This is an home page</h1>
+    <Icon icon="octicon:mark-github-16" class="text-2xl text-zinc-900"/>
   </main>
 </template>

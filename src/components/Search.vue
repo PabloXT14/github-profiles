156d00603177431be3f2af17cb6b1adcade6c340
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 07:38:17 -0300

    feat: created Search structure

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import Button from './Button.vue';
+import Input from './Input.vue';
+
+</script>
+
+<template>
+  <div class="flex gap-6">
+    <Input type="text" placeholder="Nome do repositório..." class="w-full"/>
+    <Button class="flex-1">
+      Pesquisar
+    </Button>
+  </div>
+</template>

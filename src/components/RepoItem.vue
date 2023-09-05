b00480201f881d92f78064e3d2051ba55666dd22
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 07:39:45 -0300

    feat: created RepositoriesList and RepoItem structure

---
+++ src/components/RepoItem.vue
@@ -0,0 +1,52 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import Tag from './Tag.vue';
+import { Icon } from '@iconify/vue'
+
+</script>
+
+<template>
+  <li
+    class="flex flex-col gap-3 py-5 border-b border-b-zinc-500 first:border-t first:border-t-zinc-500"
+  >
+    <a
+      href="#"
+      class="text-blue-600 text-xl font-semibold leading-normal hover:underline focus:underline"
+    >
+      Repo Title
+    </a>
+
+    <p class="text-zinc-300 text-sm leading-normal">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, eius tenetur! Laboriosam repellat qui adipisci magnam architecto quae odit quos corrupti et modi cum laborum est, quas veritatis molestias facere.
+    </p>
+
+    <div class="flex gap-[2px]">
+      <Tag variant="blue">
+        react
+      </Tag>
+      <Tag variant="blue">
+        node
+      </Tag>
+      <Tag variant="blue">
+        react-native
+      </Tag>
+    </div>
+
+    <div class="flex gap-4">
+      <div class="flex items-center gap-[2px] text-zinc-300">
+        <Icon icon="octicon:zap-16" />
+        <span class="text-xs">TypeScript</span>
+      </div>
+
+      <div class="flex items-center gap-[2px] text-zinc-300">
+        <Icon icon="octicon:repo-forked-16" />
+        <span class="text-xs">1</span>
+      </div>
+
+      <div class="flex items-center gap-[2px] text-zinc-300">
+        <Icon icon="octicon:law-16" />
+        <span class="text-xs">MIT License</span>
+      </div>
+    </div>
+  </li>
+</template>
---
+++ src/components/RepositoriesList.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import RepoItem from './RepoItem.vue';
+
+</script>
+
+<template>
+  <ul class="flex flex-col flex-1 overflow-y-auto">
+    <RepoItem />
+    <RepoItem />
+    <RepoItem />
+    <RepoItem />
+  </ul>
+</template>

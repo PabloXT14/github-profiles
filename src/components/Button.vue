afc3dca00240ef8e19fc97b5dcabfa04900a1f66
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 01:29:07 -0300

    feat: implemented button component

---
+++ src/components/Button.vue
@@ -0,0 +1,52 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { computed } from 'vue';
+import { tv } from 'tailwind-variants'
+
+interface ButtonProps {
+  as?: string | Object,
+  variant: 'green' | 'gray',
+  class?: string
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  as: 'button',
+  variant: 'green',
+})
+
+const buttonClass = computed(() => {
+  return tv({
+    base: "rounded-md px-4 h-10 flex items-center justify-center gap-2 text-base font-bold transition-all duration-150 ease-in-out",
+    variants: {
+      variant: {
+        green: [
+          "bg-green-600 text-white border border-green-600",
+          "focus:bg-green-500 focus:border-green-500",
+          "hover:bg-green-500 hover:border-green-500",
+        ],
+        gray: [
+          "bg-zinc-800 border-zinc-700 text-zinc-300 border",
+          "focus:bg-zinc-700 focus:border-zinc-500 focus:text-zinc-200",
+          "hover:bg-zinc-700 hover:border-zinc-500 hover:text-zinc-200",
+        ]
+      }
+    },
+    defaultVariants: {
+      variant: "green"
+    }
+  })({
+    variant: props.variant,
+    class: props.class
+  })
+})
+
+</script>
+
+<template>
+  <component
+    :is="props.as"
+    :class="buttonClass"
+  >
+    <slot />
+  </component>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
+
+import Button from '@/components/Button.vue';
+
 </script>
 
 <template>
   <main>
     <h1>This is an home page</h1>
-    <Icon icon="octicon:mark-github-16" class="text-2xl text-zinc-900"/>
+    <Button
+      as="button"
+      class="m-4"
+    >
+      <Icon icon="octicon:mark-github-16" />
+      <span>
+        Button
+      </span>
+    </Button>
+    <Button
+      as="button"
+      variant="gray"
+      class="m-4"
+    >
+      <Icon icon="octicon:mark-github-16" />
+      <span>
+        Button
+      </span>
+    </Button>
   </main>
 </template>

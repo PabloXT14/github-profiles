cfd35a5c275fc1667652dc40a052e8a2bf482fb6
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 12:27:30 -0300

    feat: using githubStore on RepositoriesList & RepoItem

--- src/components/RepoItem.vue
+++ src/components/RepoItem.vue
@@ -1,7 +1,19 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import type { Repository } from '@/stores/GitHubStore';
 import Tag from './Tag.vue';
 import { Icon } from '@iconify/vue'
+import { useRouter } from 'vue-router';
+
+interface RepoItemProps {
+  repo: Repository
+}
+
+const props = defineProps<RepoItemProps>()
+
+const router = useRouter()
+
+const currentRoute = router.currentRoute.value.name
 
 </script>
 
@@ -10,42 +22,54 @@ import { Icon } from '@iconify/vue'
     class="flex flex-col gap-3 py-5 border-b border-b-zinc-500 first:border-t first:border-t-zinc-500"
   >
     <a
-      href="#"
+      :href="props.repo.html_url"
       class="text-blue-600 text-xl font-semibold leading-normal hover:underline focus:underline"
+      target="_blank"
     >
-      Repo Title
+      {{ props.repo.name }}
     </a>
 
     <p class="text-zinc-300 text-sm leading-normal">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, eius tenetur! Laboriosam repellat qui adipisci magnam architecto quae odit quos corrupti et modi cum laborum est, quas veritatis molestias facere.
+      {{ props.repo.description }}
     </p>
 
     <div class="flex gap-[2px]">
-      <Tag variant="blue">
-        react
-      </Tag>
-      <Tag variant="blue">
-        node
-      </Tag>
-      <Tag variant="blue">
-        react-native
+      <Tag
+        v-for="tag in props.repo.topics"
+        :key="props.repo.id+tag.toString()"
+        variant="blue"
+      >
+        {{ tag }}
       </Tag>
     </div>
 
     <div class="flex gap-4">
-      <div class="flex items-center gap-[2px] text-zinc-300">
+      <div
+        v-if="currentRoute === 'repositories'"
+        class="flex items-center gap-[2px] text-zinc-300"
+      >
         <Icon icon="octicon:zap-16" />
-        <span class="text-xs">TypeScript</span>
+        <span class="text-xs">{{ props.repo.language }}</span>
+      </div>
+
+      <div
+        v-if="currentRoute === 'starred'"
+        class="flex items-center gap-[2px] text-zinc-300"
+      >
+        <Icon icon="octicon:zap-16" />
+        <span class="text-xs">{{ props.repo.stargazers_count }}</span>
       </div>
 
       <div class="flex items-center gap-[2px] text-zinc-300">
         <Icon icon="octicon:repo-forked-16" />
-        <span class="text-xs">1</span>
+        <span class="text-xs">{{ props.repo.forks }}</span>
       </div>
 
       <div class="flex items-center gap-[2px] text-zinc-300">
         <Icon icon="octicon:law-16" />
-        <span class="text-xs">MIT License</span>
+        <span class="text-xs">
+          {{ props.repo.license ? props.repo.license.name : 'Sem Licença' }}
+        </span>
       </div>
     </div>
   </li>
--- src/components/RepositoriesList.vue
+++ src/components/RepositoriesList.vue
@@ -1,14 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { useGitHubStore } from '@/stores/GitHubStore';
 import RepoItem from './RepoItem.vue';
+
+const githubStore = useGitHubStore();
 
 </script>
 
 <template>
   <ul class="flex flex-col flex-1 overflow-y-auto">
-    <RepoItem />
-    <RepoItem />
-    <RepoItem />
-    <RepoItem />
+    <RepoItem
+      v-for="repo in githubStore.filteredRepositories"
+      :key="repo.id"
+      :repo="repo"
+    />
   </ul>
 </template>

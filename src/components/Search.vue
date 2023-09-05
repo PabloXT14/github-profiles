fd4cdebe09caaff3ac11f67f289941448a1423a7
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 12:27:59 -0300

    feat: using githubStore on Search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,12 +2,15 @@
 <script setup lang="ts">
 import Button from './Button.vue';
 import Input from './Input.vue';
+import { useGitHubStore } from '@/stores/GitHubStore';
+
+const githubStore = useGitHubStore();
 
 </script>
 
 <template>
   <div class="flex gap-6">
-    <Input type="text" placeholder="Nome do repositório..." class="w-full"/>
+    <Input type="text" placeholder="Nome do repositório..." class="w-full" v-model="githubStore.searchFilter"/>
     <Button class="flex-1">
       Pesquisar
     </Button>

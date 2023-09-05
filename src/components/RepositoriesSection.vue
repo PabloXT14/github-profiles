7ca1c4e15225666e4745be2bc64fb5a1a5588a27
Pablo Alan <pabloxt14@gmail.com>
2023-09-05 07:39:33 -0300

    feat: created RepositoriesSection structure

---
+++ src/components/RepositoriesSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Menu from './Menu.vue';
+import RepositoriesList from './RepositoriesList.vue';
+import Search from './Search.vue';
+
+</script>
+
+<template>
+  <main class="flex gap-9 flex-col py-10 px-6 flex-1">
+    <Menu />
+    <Search />
+    <RepositoriesList />
+  </main>
+</template>
